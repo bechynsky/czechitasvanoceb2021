c2ccbaa81dd8a5fd98ed4a24558f57951f616f96
Filip Jirsák <filip@jirsak.org>
2021-11-27 13:27:24 +0100

    Test připojení do DB.

--- src/main/java/cz/czechitas/vanoceb/vanoceb/TestController.java
+++ src/main/java/cz/czechitas/vanoceb/vanoceb/TestController.java
@@ -1,6 +1,7 @@
 package cz.czechitas.vanoceb.vanoceb;
 
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,12 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Filip Jirsák
  */
 @RestController
-public class EnvController {
+public class TestController {
+
+  private final JdbcTemplate jdbcTemplate;
+
+  public TestController(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
 
   @GetMapping(produces = MediaType.TEXT_PLAIN_VALUE)
-  public String env() {
+  public String count() {
     StringBuilder builder = new StringBuilder();
-    System.getenv().keySet().forEach(key -> builder.append(key).append("\r\n"));
+    builder.append(jdbcTemplate.queryForObject("SELECT COUNT(*) FROM prani", Integer.class));
     return builder.toString();
   }
 }
