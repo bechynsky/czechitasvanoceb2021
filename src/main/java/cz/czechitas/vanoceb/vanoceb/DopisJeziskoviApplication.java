3be1b986bf757e77886cf9a1303c378b6a878416
petra <halouzkovap@seznam.cz>
2021-11-27 08:57:55 +0100

    add projekt

---
+++ src/main/java/cz/czechitas/vanoceb/vanoceb/DopisJeziskoviApplication.java
@@ -0,0 +1,13 @@
+package cz.czechitas.vanoceb.vanoceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopisJeziskoviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DopisJeziskoviApplication.class, args);
+	}
+
+}
